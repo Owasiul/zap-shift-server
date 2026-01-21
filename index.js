7f295ce4bba27e027cca0f8b8cca56f58e2ad2f5
Owasiul <sanisuper0@gmail.com>
2026-01-21 16:16:36 +0600

    adding MongoDB and createing paths

--- index.js
+++ index.js
@@ -3,17 +3,64 @@ const app = express();
 const port = process.env.PORT || 3000;
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 // middleware
 app.use(express.json());
 app.use(cors);
 
-// listeners
-app.get("/", (req, res) => {
-  res.json({ status: "ok", message: "zap shift server" });
+const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Pass}@cluster0.ldvla9s.mongodb.net/?appName=Cluster0`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
-app.listen(port, () => {
-  console.log(`Zap shift server is listening ${port}`);
-  console.log(`Zap shift server Connect with DB`);
+// listeners
+client.connect().then(() =>
+  app.listen(port, () => {
+    console.log(`Zap shift server is listening ${port}`);
+    console.log(`Zap shift server Connect with DB`);
+  }),
+);
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    // database name
+    const zapShiftDB = client.db("zapShiftDB");
+    const parcelCollections = zapShiftDB.collection("parcels");
+
+    // parcels api
+    app.get("/parcels", async (req, res) => {});
+    app.post("/parcels", async (req, res) => {
+      try {
+        const parcel = req.body;
+        const result = await parcelCollections.insertOne(parcel);
+        res.send(result);
+      } catch (error) {
+        console.log(error);
+      }
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.json({ status: "ok", message: "zap shift server" });
 });
