b59ea56fd8980837c7008d47a1c89db7977900c3
Owasiul <sanisuper0@gmail.com>
2026-01-22 23:11:45 +0600

    added payment success method

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
+const stripe = require("stripe")(process.env.Stripe_key);
 
 // middleware
 app.use(express.json());
@@ -29,7 +30,7 @@ client
     const parcelCollections = zapShiftDB.collection("parcels");
 
     // parcels api
-    app.get("/parcel", async (req, res) => {
+    app.get("/all-parcel", async (req, res) => {
       try {
         const parcels = await parcelCollections.find().toArray();
         parcels.createdAt = new Date();
@@ -56,7 +57,18 @@ client
         res.status(500).send({ error: "Failed to fetch parcels" });
       }
     });
-
+    // get via id
+    app.get("/parcels/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await parcelCollections.findOne(query);
+        res.send(result);
+      } catch (error) {
+        console.log(error);
+        res.status(500).send({ error: "Failed to find parcel" });
+      }
+    });
     app.post("/parcels", async (req, res) => {
       try {
         const parcel = req.body;
@@ -66,6 +78,72 @@ client
         console.log(error);
         res.status(500).send({ error: "Failed to create parcel" });
       }
+    });
+
+    app.delete("/parcels/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await parcelCollections.deleteOne(query);
+        res.send(result);
+      } catch (error) {
+        console.log(error);
+        res.status(500).send({ error: "Failed to delete parcel" });
+      }
+    });
+
+    // payment related api
+    const YOUR_DOMAIN = process.env.Stripe_Domain;
+    app.post("/create-checkout-session", async (req, res) => {
+      try {
+        const paymentInfo = req.body;
+        const amount = Math.round(paymentInfo.cost) * 100;
+
+        const session = await stripe.checkout.sessions.create({
+          line_items: [
+            {
+              price_data: {
+                currency: "bdt",
+                unit_amount: amount,
+                product_data: {
+                  name: paymentInfo["parcel-name"],
+                },
+              },
+              quantity: 1,
+            },
+          ],
+          mode: "payment",
+          metadata: {
+            parcelId: paymentInfo.parcelId,
+          },
+          success_url: `${YOUR_DOMAIN}/dashboard/payment-success?session_id={CHECKOUT_SESSION_ID}`,
+          cancel_url: `${YOUR_DOMAIN}/dashboard/payment-cancelled`,
+        });
+
+        // Only send JSON response - remove the redirect
+        res.send({ url: session.url });
+      } catch (error) {
+        console.error("Payment error:", error);
+        res.status(500).send({ error: error.message });
+      }
+    });
+
+    app.patch("/verify-payment-success", async (req, res) => {
+      const sessionId = req.query.session_id;
+      const seession = await stripe.checkout.sessions.retrieve(sessionId);
+      console.log("seession retrive", seession);
+      if (seession.payment_status === "paid") {
+        const id = seession.metadata.parcelId;
+        const query = { _id: new ObjectId(id) };
+        const update = {
+          $set: {
+            payment_status: "paid",
+          },
+        };
+        const result = await parcelCollections.updateOne(query, update);
+        res.send(result);
+      }
+      res.send({ success: false });
     });
 
     // Send a ping to confirm a successful connection
