8787134dd79bae0331df4ef561e72348efa07619
Owasiul <sanisuper0@gmail.com>
2026-01-24 22:28:00 +0600

    adding Tracking and transaction Id

--- index.js
+++ index.js
@@ -12,6 +12,19 @@ app.use(cors());
 
 const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Pass}@cluster0.ldvla9s.mongodb.net/?appName=Cluster0`;
 
+const { customAlphabet } = require("nanoid");
+const nanoid = customAlphabet("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ", 8);
+
+function generateTrackingId() {
+  const prefix = "ZS";
+  const year = new Date().getFullYear().toString().slice(-2); // Last 2 digits
+  const month = (new Date().getMonth() + 1).toString().padStart(2, "0");
+  const uniqueId = nanoid();
+
+  return `${prefix}${year}${month}${uniqueId}`;
+  // Example: ZS2501A7K9M2N4
+}
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri);
 
@@ -28,6 +41,7 @@ client
     // database name
     const zapShiftDB = client.db("zapShiftDB");
     const parcelCollections = zapShiftDB.collection("parcels");
+    const paymentColllection = zapShiftDB.collection("payments");
 
     // parcels api
     app.get("/all-parcel", async (req, res) => {
@@ -98,6 +112,7 @@ client
       try {
         const paymentInfo = req.body;
         const amount = Math.round(paymentInfo.cost) * 100;
+        // console.log({amount});
 
         const session = await stripe.checkout.sessions.create({
           line_items: [
@@ -115,6 +130,7 @@ client
           mode: "payment",
           metadata: {
             parcelId: paymentInfo.parcelId,
+            parcelName: paymentInfo["parcel-name"],
           },
           success_url: `${YOUR_DOMAIN}/dashboard/payment-success?session_id={CHECKOUT_SESSION_ID}`,
           cancel_url: `${YOUR_DOMAIN}/dashboard/payment-cancelled`,
@@ -130,20 +146,50 @@ client
 
     app.patch("/verify-payment-success", async (req, res) => {
       const sessionId = req.query.session_id;
-      const seession = await stripe.checkout.sessions.retrieve(sessionId);
-      console.log("seession retrive", seession);
-      if (seession.payment_status === "paid") {
-        const id = seession.metadata.parcelId;
+      const session = await stripe.checkout.sessions.retrieve(sessionId);
+      const TrackingId = generateTrackingId();
+      // console.log("seession retrive", session);
+      if (session.payment_status === "paid") {
+        const id = session.metadata.parcelId;
         const query = { _id: new ObjectId(id) };
         const update = {
           $set: {
             payment_status: "paid",
+            trackingId: TrackingId,
           },
         };
         const result = await parcelCollections.updateOne(query, update);
-        res.send(result);
+
+        const paymentHistory = {
+          amount: session.amount_total / 100,
+          currency: session.currency,
+          email: session.customer_details.email,
+          parcelId: session.metadata.parcelId,
+          parcelName: session.metadata.parcelName,
+          transactionId: session.payment_intent,
+          PaymentStatus: session.payment_status,
+          paidAt: new Date(),
+        };
+
+        // console.log(paymentHistory);
+        if (session.payment_status === "paid") {
+          const resultPayment = await paymentColllection.updateOne(
+            { transactionId: session.payment_intent },
+            { $set: paymentHistory },
+            { trackingId: TrackingId },
+          );
+          console.log(resultPayment);
+          res.send({
+            success: true,
+            transactionId: session.payment_intent,
+            trackingId: TrackingId,
+            modifyPayment: result,
+            paymentInfo: resultPayment,
+          });
+        } else {
+          res.send({ success: false, message: "Payment Failed" });
+        }
       }
-      res.send({ success: false });
     });
 
     // Send a ping to confirm a successful connection
